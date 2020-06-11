993187257474f47d49e3ac623c2adaeda74e7987
DAVIDDAIJUNCHEN <673848108@qq.com>
2020-06-11 22:36:54 +0800

    update the old feature script to make them compatible with new data_preocessing.py

--- 诈骗电话识别/daijun/test_idcard_cnt-avg_arpu-std_arpu-call_dur-called_people-call_type-sms_people-sms_type-sms_days.py
+++ 诈骗电话识别/daijun/test_idcard_cnt-avg_arpu-std_arpu-call_dur-called_people-call_type-sms_people-sms_type-sms_days.py
@@ -51,6 +51,119 @@ with open('./test_results/'+feauture_name+'/'+'logisticRegression.csv', 'w', new
     for phone, pred in zip(phone_no_m, pred_test):
         writer.writerow({'phone_no_m': phone, 'label': pred})
 
+# Model II: Logistic regression CV
+clf_logistRegCV = LogisticRegressionCV(cv=5, random_state=0).fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_logistRegCV.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Logistic Regression CV=5')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_logistRegCV.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'LogisticRegressionCV5.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model III: percepton
+from sklearn.linear_model import Perceptron
+clf_perceptron = Perceptron(tol=1e-3, random_state=0)
+clf_perceptron.fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_perceptron.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Perceptron')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_perceptron.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'percepton.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model IV: RidgeClassifier
+from sklearn.linear_model import RidgeClassifier
+clf_ridgeClassifier = RidgeClassifier().fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_ridgeClassifier.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Ridge Classification')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_ridgeClassifier.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'RidgeClassifier.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model V: AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier
+clf_AdaBoost = AdaBoostClassifier(n_estimators=100, random_state=0)
+clf_AdaBoost.fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_AdaBoost.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Adaboost')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_AdaBoost.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'Adaboost.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model VI: BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.svm import SVC
+clf_Bagging = BaggingClassifier(base_estimator=SVC(), n_estimators=10, random_state=0)
+clf_Bagging.fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_Bagging.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Bagging_SVC')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_Bagging.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'BaggingClassifier.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model VII: random forest
+from sklearn.ensemble import RandomForestClassifier
+clf_randforest = RandomForestClassifier(max_depth=2, random_state=0)
+clf_randforest.fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_randforest.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='Random forest')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_randforest.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'RandomForest.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+
 # Model VIII: Gradientboosting
 from sklearn.ensemble import GradientBoostingClassifier
 clf_gradboost = GradientBoostingClassifier(max_depth=2, random_state=0)
@@ -64,6 +177,47 @@ evaluate(label_dev, pred_dev, model='Gradient boosting')
 pred_test = clf_gradboost.predict(X_test).tolist()
 
 with open('./test_results/'+feauture_name+'/'+'Gradientboosting.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+
+# Model IX: MLP Classifier
+from sklearn.neural_network import MLPClassifier
+clf_mlp = MLPClassifier(random_state=1, max_iter=300).fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_mlp.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='MLP')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_mlp.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'MLP.csv', 'w', newline='', encoding='utf-8') as fout:
+    field_names = ['phone_no_m', 'label']
+    writer = csv.DictWriter(fout, fieldnames=field_names)
+    writer.writeheader()
+    for phone, pred in zip(phone_no_m, pred_test):
+        writer.writerow({'phone_no_m': phone, 'label': pred})
+
+# Model X: SVM
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+clf_svm = make_pipeline(StandardScaler(), LinearSVC(random_state=0, tol=1e-5))
+clf_svm.fit(X_train, label_train)
+
+# evaluate toy model on (X_dev, label_dev)
+pred_dev = clf_svm.predict(X_dev).tolist()
+evaluate(label_dev, pred_dev, model='SVM')
+
+# evaluate toy model on (X_test, label_test)
+pred_test = clf_svm.predict(X_test).tolist()
+
+with open('./test_results/'+feauture_name+'/'+'SVM.csv', 'w', newline='', encoding='utf-8') as fout:
     field_names = ['phone_no_m', 'label']
     writer = csv.DictWriter(fout, fieldnames=field_names)
     writer.writeheader()
