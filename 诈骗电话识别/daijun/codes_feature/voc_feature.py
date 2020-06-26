32c7524e857470a39b02ad008e635c88604954f7
DAVIDDAIJUNCHEN <673848108@qq.com>
2020-06-26 14:59:22 +0800

    2nd version codes with config.yml file

--- 诈骗电话识别/daijun/codes_feature/voc_feature.py
+++ 诈骗电话识别/daijun/codes_feature/voc_feature.py
@@ -1,31 +1,6 @@
 #/usr/bin/env python3
 
 import pandas as pd
-import time
-
-# def called_people(dataframe_phone_no, arguments):
-#     """return number of called people in given months"""
-#     # convert to datetime format
-#     t0 = time.time()
-#     months = arguments['months']
-#     months = pd.DataFrame(months, columns=['year_month_of_date'])
-#     months['year_month_of_date'] = pd.to_datetime(months['year_month_of_date'],
-#                                                   format='%Y-%m')
-#     months['year_month_of_date'] = months.year_month_of_date.dt.to_period('M')
-#
-#     voc_dataframe = dataframe_phone_no['voc']
-#     voc_dataframe['start_datetime'] = pd.to_datetime(voc_dataframe['start_datetime'],
-#                                                      format='%Y%m%d %H:%M:%S')
-#
-#     voc_dataframe['year_month_of_date'] = voc_dataframe.start_datetime.dt.to_period('M')
-#     check = voc_dataframe['year_month_of_date'].isin(months['year_month_of_date'])
-#     print('t0:', time.time() - t0)
-#     voc_df_months = voc_dataframe[check]
-#
-#     #voc_df_months = voc_dataframe.loc[lambda df: df['year_month_of_date'].dt.strftime('%Y-%m').isin(months), :]
-#     called_people = set(voc_df_months['opposite_no_m'])
-#
-#     return len(called_people)
 
 def called_people(dataframe_phone_no, arguments):
     months = arguments['months']
