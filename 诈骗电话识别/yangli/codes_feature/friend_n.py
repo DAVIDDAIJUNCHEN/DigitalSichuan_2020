573d6a5ed741beda993f6c61221869d7ddc42bc9
DAVIDDAIJUNCHEN <673848108@qq.com>
2020-06-30 08:54:09 +0800

    upgrade script to include model building with different months and ensemble method

--- 诈骗电话识别/yangli/codes_feature/friend_n.py
+++ 诈骗电话识别/yangli/codes_feature/friend_n.py
@@ -6,22 +6,21 @@ def friends_n(dataframe_phone_no, arguments):
     """return number of friends in given months"""
     # convert to datetime format
     months = arguments['months']
+    months_regex = '|'.join(months)
     voc_dataframe = dataframe_phone_no['voc']
-    voc_dataframe['start_datetime'] = pd.to_datetime(voc_dataframe['start_datetime'],
-                                                     format='%Y%m%d %H:%M:%S')
-    voc_dataframe['year_month_of_date'] = voc_dataframe.start_datetime.dt.to_period('M')
-    voc_df_months = voc_dataframe.loc[lambda df: df['year_month_of_date'].dt.strftime('%Y-%m').isin(months), :]
+    voc_df_months = voc_dataframe[voc_dataframe['start_datetime'].str.contains(months_regex)]
     called_people = list(voc_df_months['opposite_no_m'])
     called_people_dic={}
     for item_str in called_people:
-        if item_str not in called_people_dic:
-           called_people_dic[item_str]=1
+        if item_str not in called_people_dic.keys():
+           called_people_dic[item_str] = 1
         else:
-           called_people_dic[item_str]+=1
-    friend=[]
-    for k in called_people_dic:
-        friend.append(k)
-    for k in friend
-        if called_people_dic[k]=1,2,3,4
-          del friend[k]
-    return len(friend)
+           called_people_dic[item_str] += 1
+
+    friends = []
+
+    for k in called_people_dic.keys():
+        if called_people_dic[k] > 4:
+            friends.append(k)
+
+    return len(friends)
--- 诈骗电话识别/yangli/codes_feature/short_call.py
+++ 诈骗电话识别/yangli/codes_feature/short_call.py
@@ -6,11 +6,8 @@ def called_people(dataframe_phone_no, arguments):
     """return number of short call in given months"""
     # convert to datetime format
     months = arguments['months']
+    months_regex = '|'.join(months)
     voc_dataframe = dataframe_phone_no['voc']
-    voc_dataframe['start_datetime'] = pd.to_datetime(voc_dataframe['start_datetime'],
-                                                     format='%Y%m%d %H:%M:%S')
-    voc_dataframe['year_month_of_date'] = voc_dataframe.start_datetime.dt.to_period('M')
-    voc_df_months = voc_dataframe.loc[lambda df: df['year_month_of_date'].dt.strftime('%Y-%m').isin(months), :]
-    short_call=voc_df_months.df[[each<6 for each in voc_df_months['call_dur'] ]]
-
+    voc_df_months = voc_dataframe[voc_dataframe['start_datetime'].str.contains(months_regex)]
+    short_call = [1 for each in voc_df_months['call_dur'] if each < 6]
     return len(short_call)
