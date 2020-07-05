260a8cf60fca1552f6341cd7606e30ea3c39a090
YoungLeei <66346530+YoungLeei@users.noreply.github.com>
2020-07-05 16:33:23 +0800

    a

---
+++ 诈骗电话识别/yangli/codes_feature/sms_friend_liyang.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from collections import Counter
+
+def ratio_sms_friends(dataframe_phone_no, arguments):
+    """return number of friends in given months"""
+    # convert to datetime format
+    months = arguments['months']
+    thres_friend = arguments['threshold_friend']
+    months_regex = '|'.join(months)
+    sms_dataframe = dataframe_phone_no['sms']
+    sms_df_months = sms_dataframe[sms_dataframe['request_datetime'].str.contains(months_regex)]
+    smsed_people = list(sms_df_months['opposite_no_m'])
+    smsed_people_dict = dict(Counter(smsed_people))
+    sms_friend = 0
+
+    for opposite_phone, times in smsed_people_dict.items():
+        if times >= thres_friend:
+            sms_friend += 1
+    # if no smsed people in these months, assign 100%
+    if len(smsed_people) == 0:
+        return 1.0
+
+    return sms_friend / len(smsed_people_dict)
+
+def test():
+    data_sms = [['f0ebee98809cb1a9', 1, '2019-12-25 21:26:40'],
+                ['dedd4a48c3a8f', 1, '2020-01-02 20:14:33'],
+                ['dedd4a48c3a8f', 1, '2020-01-02 20:14:33']]
+
+    sms_df = pd.DataFrame(data_sms, columns=['opposite_no_m','calltype_id','request_datetime'])
+    dataframe_phone_no = {'sms': sms_df}
+    arguments = {"months": ['2019-12', '2020-01'], 'threshold_friend': 2}
+    print(ratio_sms_friends(dataframe_phone_no, arguments))
+
+if __name__ == '__main__':
+    test()
